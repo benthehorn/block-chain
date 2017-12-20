80a2be40b7d604fed08725cb689fa76784ef1441
benthehorn <benhorn13@googlemail.com>
2017-12-20 22:16:52 +0100

    read me

--- autoscenario.sh
+++ autoscenario.sh
@@ -15,3 +15,21 @@ echo " Now we will run the docker compose file."
 docker-compose up -d
 echo " We will now see the 4 peers being initiated."
 echo " "
+echo " To see the test blocks created :"
+curl http://localhost:3001/blocks
+echo " "
+echo " "
+echo " "
+echo " "
+echo " Lets add a block :"
+curl -H "Content-type:application/json" --data '{"data" : "A New Block"}' http://localhost:3001/mineBlock
+echo " "
+echo " And list the blocks including the new one."
+echo " "
+curl http://localhost:3001/blocks
+echo " That shows that we can add blocks to our chain."
+docker-compose down
+docker rmi benthehorn/block-chain-project:latest -f
+rm ./docker-compose.yml
+echo " "
+echo "The End. "
