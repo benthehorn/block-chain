a54663c2c09d0e80b3dbdb45e2cb110130977973
benthehorn <benhorn13@googlemail.com>
2017-12-20 22:01:51 +0100

    read me

--- autoscenario.sh
+++ autoscenario.sh
@@ -9,5 +9,9 @@ echo " "
 echo " Now we will download the docker compose file."
 echo " "
 wget https://raw.githubusercontent.com/benthehorn/block-chain/master/docker-compose.yml
+echo " If you have docker and docker-compose installed, press enter to continue."
+read -p "Press ENTER now."
 echo " Now we will run the docker compose file."
 docker-compose up -d
+echo " We will now see the 4 peers being initiated."
+echo " "
